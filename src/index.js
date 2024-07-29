92f7b06906d4b2f6b1f54baf7c1a5654741923fb
ruslan898 <ruslan_abibullaev@mail.ru>
2024-07-29 21:53:38 +0500

    complete reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    const num = Math.abs(n);
+    const str = num.toString();
+    let result = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        result += str[i];
+    }
+
+    return result;
 }
